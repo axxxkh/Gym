17f9eccb1b4cd1cebe7d54f69f4c14c283549556
axxxkh <alxxxkh@gmail.com>
2023-06-20 22:11:23 +0300

    adding security

--- admin-service/src/main/java/org/example/clientService/ClientApp.java
+++ admin-service/src/main/java/org/example/clientService/ClientApp.java
@@ -5,12 +5,13 @@ import org.example.clientService.service.ClientService;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ClientApp {
 
     @Bean
